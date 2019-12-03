a02f9a552612119ca21f5c86aa3d403f977adff6
chokchai9900 <chokchai9900@gmail.com>
2019-12-03 14:37:42 +0700

    10

---
+++ GraphQLTest/Controllers/AuthorsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQLTest.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphQLTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : Controller
+    {
+        private readonly BlogService blogService;
+        public AuthorsController(BlogService blogService)
+        {
+            this.blogService = blogService;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return new ObjectResult(blogService.GetAllAuthors());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetAuthorById(int id)
+        {
+            return new ObjectResult(blogService.GetAuthorById(id));
+        }
+        [HttpGet("{id}/posts")]
+        public IActionResult GetPostsByAuthor(int id)
+        {
+            return new ObjectResult(blogService.GetPostsByAuthor(id));
+        }
+        [HttpGet("{id}/socials")]
+        public IActionResult GetSocialsByAuthor(int id)
+        {
+            return new ObjectResult(blogService.GetSNsByAuthor(id));
+        }
+    }
+}
---
+++ GraphQLTest/Controllers/GraphQlController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+using GraphQLTest.Queries;
+using GraphQLTest.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphQLTest.Controllers
+{
+    [Route(Startup.GraphQlPath)]
+    public class GraphQlController : Controller
+    {
+        readonly BlogService blogService;
+        public GraphQlController(BlogService blogService)
+        {
+            this.blogService = blogService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GraphQlQuery query)
+        {
+            var schema = new Schema { Query = new AuthorQuery(blogService) };
+            var result = await new DocumentExecuter().ExecuteAsync(x =>
+            {
+                x.Schema = schema;
+                x.Query = query.Query;
+                x.Inputs = query.Variables;
+            });
+            if (result.Errors?.Count > 0)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+    }
+}
